041861b317e8986fc953415f2368f01017b6b43d
Sarah Habib <habibsarah11@gmail.com>
2022-05-29 19:26:35 -0700

    fish can move up and down

---
+++ FishRacing/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0, verticalInput, 0);
+        transform.Translate(movement);
+    }
+}
